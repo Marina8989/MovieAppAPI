4479a01c2e5bf61e960dae5f195f17f79a9693ac
Marina8989 <marina.mill209@gmail.com>
2021-06-01 14:29:46 -0700

    added some of the url links required

---
+++ main.js
@@ -0,0 +1,3 @@
+const API_KEY = 'api_key=4c3ad92dbbd8b98710480f66cb2e91bf';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
