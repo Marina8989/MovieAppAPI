d95b19d951703afec53bf374c52b99e89e139e5a
Marina8989 <marina.mill209@gmail.com>
2021-06-01 15:20:47 -0700

    added list of movies to page and impletemented a search button

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
 const main = document.getElementById('main');
+const SEARCH_URL = '/search/movie?';
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
 
 
 getMovie(API_URL);
 
 function getMovie(url) {
      fetch(url).then(res => res.json()).then(data => {
-         console.log(data.results);
          showMovie(data.results);
      })
 }
@@ -17,7 +21,7 @@ function getMovie(url) {
 function showMovie(data) {
     main.innerText = '';
 
-   data.forEach(movie => {
+    data.forEach(movie => {
        const {title, poster_path, vote_average, overview} = movie;
 
        const movieDiv = document.createElement('div');
@@ -47,3 +51,14 @@ function getVote(vote) {
          return 'red';
      }
 }
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+   const searchTerm = search.value;
+     if(searchTerm) {
+        getMovie(BASE_URL +'/search/movie?'+API_KEY +'&query='+searchTerm);
+        search.value = '';
+     }else{
+         getMovie(API_URL);
+     }
+})
